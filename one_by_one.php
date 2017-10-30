765a148cafaece44b2fbfc7784bce9d3a7c147d3
Emailvalidation <33231996+Xavierluijer@users.noreply.github.com>
2017-10-30 19:59:54 +0000

    Create one_by_one.php

---
+++ one_by_one.php
@@ -0,0 +1,11 @@
+$email = "test@example.com";
+$key = "PUT YOUR API KEY HERE";
+$url = "https://app.emailvalidation.fr/api/verifEmail?secret=".$key."&email=".$email;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+$response = curl_exec($ch);
+echo $response;
+curl_close($ch);
