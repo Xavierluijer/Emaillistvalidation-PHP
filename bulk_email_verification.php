086719d4ae89bda7c7d7577a0f3ffdffdacf3ab0
Emailvalidation <33231996+Xavierluijer@users.noreply.github.com>
2017-11-02 12:44:34 +0000

    Update bulk_email_verification.php

--- bulk_email_verification.php
+++ bulk_email_verification.php
@@ -1,7 +1,7 @@
 <!-- upload the file -->
 $key = "PUT YOUR API KEY HERE";
 $settings['file_contents'] ="@/home/Downloads/emails.txt"; //path to your file
-$url = 'https://apps.emailvalidation.fr/api/verifApiFile?secret='.$key.'&filename=my_emails.txt';
+$url = 'https://apps.emaillistvalidation.com/api/verifApiFile?secret='.$key.'&filename=my_emails.txt';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -12,6 +12,6 @@ curl_close($ch);
 
 <!-- download ready file -->
 $key = "PUT YOUR API KEY HERE";
-            $url = 'https://apps.emailvalidation.fr/api/getApiFileInfo?secret='.$key.'&id=10700';
+            $url = 'https://apps.emaillistvalidation.com/api/getApiFileInfo?secret='.$key.'&id=10700';
             $string = file_get_contents($url);
             list($file_id,$filename,$unique,$lines,$lines_processed,$status,$timestamp,$link1,$link2) = explode('|',$string); //parse data
