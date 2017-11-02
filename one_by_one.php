9201484f6fab6a933cd292544c6a3f0f71cb21ee
Emailvalidation <33231996+Xavierluijer@users.noreply.github.com>
2017-11-02 12:45:24 +0000

    Update one_by_one.php

--- one_by_one.php
+++ one_by_one.php
@@ -1,6 +1,6 @@
 $email = "test@example.com";
 $key = "PUT YOUR API KEY HERE";
-$url = "https://app.emailvalidation.fr/api/verifEmail?secret=".$key."&email=".$email;
+$url = "https://app.emaillistvalidation.com/api/verifEmail?secret=".$key."&email=".$email;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
